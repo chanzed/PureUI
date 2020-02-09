38a35b0343c8b435060951afa89fdf55b27bbc81
chanzed <chanzedcz@gmail.com>
2020-02-10 03:17:06 +0800

    将button改为单文件组件

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Button from './components/button/button.vue'
+
+Vue.component('p-button', Button)
+new Vue({
+    el: '#app',
+})
---
+++ src/components/button/button.vue
@@ -0,0 +1,37 @@
+<template>
+    <button class='p-button'>按钮</button>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='scss'>
+$button-height: 32px;
+$font-size: 14px;
+$button-bg: white;
+$button-active-bg: #eee;
+$border-radius: 4px;
+$color: #333;
+$border-color: #999;
+$border-color-hover: #666;
+.p-button {
+  font-size: $font-size;
+  height: $button-height;
+  padding: 0 1em;
+  border-radius: $border-radius;
+  border: 1px solid $border-color;
+  background: $button-bg;
+  &:hover {
+    border-color: $border-color-hover;
+  }
+
+  &:active {
+    background-color: $button-active-bg;
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
