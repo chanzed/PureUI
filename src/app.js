ecbb2beacb52e8f5aba4105844a0f5fc28f1a160
chanzed <chanzedcz@gmail.com>
2020-02-10 19:21:41 +0800

    安装chai

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Button from './components/button/button.vue'
 import Icon from './components/icon/icon.vue'
+import ButtonGroup from './components/button/buttonGroup.vue'
 
 Vue.component('p-button', Button)
 Vue.component('p-icon', Icon)
+Vue.component('p-button-group', ButtonGroup)
 new Vue({
     el: '#app',
+    data: {
+        loading1: true,
+    }
 })
