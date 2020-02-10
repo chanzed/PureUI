b196c80fac43d446466f059f3415eb549ec95fe0
chanzed <chanzedcz@gmail.com>
2020-02-10 20:54:16 +0800

    编写p-button组件测试用例

--- src/app.js
+++ src/app.js
@@ -12,3 +12,95 @@ new Vue({
         loading1: true,
     }
 })
+
+//unit test
+import chai from 'chai'
+import spies from 'chai-spies'
+const expect = chai.expect
+chai.use(spies)
+
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    //把button挂载到内存里
+    button.$mount()
+    const useElement = button.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+    button.$el.remove()
+    button.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true,
+        }
+    })
+    //把button挂载到内存里
+    button.$mount()
+    const useElement = button.$el.querySelector('use')
+    const href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const button = new Constructor({
+        propsData: {
+            icon: 'loading',
+            iconPosition: 'right',
+        }
+    })
+    //把button挂载到内存里
+    button.$mount(div)
+    const svg = button.$el.querySelector('svg')
+    const { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'loading',
+            iconPosition: 'left',
+        }
+    })
+    //把button挂载到内存里
+    vm.$mount(div)
+    const svg = vm.$el.querySelector('svg')
+    const { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'loading',
+        }
+    })
+    //把button挂载到内存里
+    vm.$mount()
+    const button = vm.$el
+    const spy = chai.spy(()=>{})
+    vm.$on('click', spy)
+    button.click()
+    expect(spy).to.have.been.called()
+    vm.$el.remove()
+    vm.$destroy()
+}
