faccbaf19d818c2b6a4838e4650b3dad57180218
chanzed <chanzedcz@gmail.com>
2020-02-10 16:38:04 +0800

    添加iconPosition属性，控制icon的位置

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,9 +1,27 @@
 <template>
-    <button class='p-button'>按钮</button>
+  <button class="p-button" :class="`icon-${iconPosition}`">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`" />
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    icon: String,
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator: function (value) {
+        return ['left', 'right'].includes(value)
+      }
+    }
+  },
+}
 </script>
 
 <style lang='scss'>
@@ -18,10 +36,14 @@ $border-color-hover: #666;
 .p-button {
   font-size: $font-size;
   height: $button-height;
-  padding: 0 1em;
+  padding: 0 0.7em;
   border-radius: $border-radius;
   border: 1px solid $border-color;
   background: $button-bg;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: top;
   &:hover {
     border-color: $border-color-hover;
   }
@@ -32,6 +54,28 @@ $border-color-hover: #666;
 
   &:focus {
     outline: none;
+  }
+  .icon {
+    width: 1em;
+    height: 1em;
+  }
+  &.icon-left {
+    .content {
+      order: 2;
+    }
+    .icon {
+      order: 1;
+      margin-right: 0.3em;
+    }
+  }
+  &.icon-right {
+    .content {
+      order: 1;
+    }
+    .icon {
+      order: 2;
+      margin-left: 0.3em;
+    }
   }
 }
 </style>
