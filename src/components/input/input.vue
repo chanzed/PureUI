fb6df54c1bb9aa2815acab5317ae5ac7d37f31c8
chanzed <chanzedcz@gmail.com>
2020-02-11 18:12:49 +0800

    完成input的基本样式

---
+++ src/components/input/input.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input type="text" :disabled="disabled" :value="value" :readonly="readonly" />
+    <template v-if="error">
+      <p-icon class="error-icon" name="error"></p-icon>
+      <span class="error-message">{{error}}</span>
+    </template>
+  </div>
+</template>
+
+<script>
+import Icon from '../icon/icon'
+export default {
+  components: {
+    'p-icon': Icon,
+  },
+  props: {
+    value: {
+      type: String,
+    },
+    readonly: {
+      type: Boolean,
+    },
+    disabled: {
+      type: Boolean,
+    },
+    error: {
+      type: String,
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+$font-size: 12px;
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #f14500;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  > input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+    font-size: inherit;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+    &[readonly],
+    &[disabled] {
+      cursor: not-allowed;
+      border-color: #bbb;
+      color: #bbb;
+    }
+  }
+  &.error {
+    > input {
+      border-color: $red;
+    }
+    > :not(:last-child) {
+      margin-right: 0.5em;
+    }
+    .error-icon {
+      fill: $red;
+    }
+    .error-message {
+      color: $red;
+    }
+  }
+}
+</style>
