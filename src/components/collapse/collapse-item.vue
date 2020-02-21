015a07eb519cb1c83234133c06dc416f98cb6c7f
chanzed <chanzedcz@gmail.com>
2020-02-21 23:24:20 +0800

    完成collapse

---
+++ src/components/collapse/collapse-item.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="collapse-item">
+    <div class="title" @click="toggle">{{title}}</div>
+    <div class="content" v-if="open">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "PureCollapseItem",
+  inject: ['eventBus'],
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+    }
+  },
+  data() {
+    return {
+      open: false,
+    }
+  },
+  mounted() {
+    this.eventBus.$on('update:selected', (names) => {
+        if (names.indexOf(this.name) !== -1) {
+          this.open = true
+        } else {
+          this.open = false
+        }
+    })
+  },
+  methods: {
+    toggle() {
+      if (this.open) {
+        this.eventBus.$emit('removeSelected', this.name)
+      } else {
+        this.eventBus.$emit('addSelected', this.name)
+      }
+    },
+  }
+};
+</script>
+<style lang="scss" scoped>
+$grey: #ddd;
+$border-radius: 4px;
+$border-width: 1px;
+.collapse-item {
+  > .title {
+    border: $border-width solid $grey;
+    margin-top: -$border-width;
+    margin-left: -$border-width;
+    margin-right: -$border-width;
+    min-height: 32px;
+    display: flex;
+    align-items: center;
+    padding: 0 1em;
+  }
+  &:first-child {
+    > .title {
+    border-top-left-radius: $border-radius;
+    border-top-right-radius: $border-radius;
+    }
+  }
+  > .content {
+    padding: 8px;
+  }
+}
---
+++ src/components/collapse/collapse.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+<script>
+import Vue from "vue";
+export default {
+  name: "PureCollapse",
+  props: {
+    single: {
+      type: Boolean,
+      default: false,
+      validator(value) {
+        return [false, true].indexOf(value) !== -1;
+      }
+    },
+    selected: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      eventBus: new Vue()
+    };
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus
+    };
+  },
+  mounted() {
+    this.eventBus.$emit("update:selected", this.selected);
+    this.eventBus.$on("removeSelected", name => {
+      let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+      let index = selectedCopy.indexOf(name);
+      selectedCopy.splice(index, 1);
+      this.eventBus.$emit("update:selected", selectedCopy);
+      this.$emit("update:selected", selectedCopy);
+    });
+    this.eventBus.$on("addSelected", name => {
+      let selectedCopy = JSON.parse(JSON.stringify(this.selected))
+      if (this.single) {
+        selectedCopy = [name];
+      } else {
+        selectedCopy.push(name);
+      }
+      this.eventBus.$emit("update:selected", selectedCopy);
+      this.$emit("update:selected", selectedCopy);
+    });
+  }
+};
+</script>
+<style lang="scss" scoped>
+$grey: #ddd;
+$border-radius: 4px;
+$border-width: 1px;
+.collapse {
+  border: $border-width solid $grey;
+  border-radius: $border-radius;
+}
